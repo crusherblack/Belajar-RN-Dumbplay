e880b2c566f525c5234f403f8b5c01730fcaef43
crusherblack <fadhildarma13@gmail.com>
2020-06-28 20:36:08 +0700

    fix structure folder

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import MyTabs from './components/BottomTabs';
+import MyTabs from './src/components/BottomTabs';
 
 const App = () => {
 	return (
