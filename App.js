ddb7280b7568efe527b646509858554b9e6665db
crusherblack <fadhildarma13@gmail.com>
2020-06-29 19:00:31 +0700

    add simple splashscreen

--- App.js
+++ App.js
@@ -1,34 +1,35 @@
-import React, {useState} from 'react';
-import {Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import React, { useState } from 'react';
+import { Text, MaskedViewIOS } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import MainNavigator from './src/navigators/BottomTabs';
-import {AuthStackNavigator} from './src/navigators/AuthNavigator';
+import { AuthStackNavigator } from './src/navigators/AuthNavigator';
 
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const RootStack = createStackNavigator();
 
 const App = () => {
-  const auth = useSelector((state) => state.auth);
-  const {isAuthenticated, loading} = auth;
+	const auth = useSelector((state) => state.auth);
+	const { isAuthenticated, loading } = auth;
 
-  return (
-    <NavigationContainer>
-      <RootStack.Navigator
-        screenOptions={{
-          headerShown: false,
-          animationEnabled: false,
-        }}>
-        {isAuthenticated ? (
-          <RootStack.Screen name={'MainStack'} component={MainNavigator} />
-        ) : (
-          <RootStack.Screen name={'AuthStack'} component={AuthStackNavigator} />
-        )}
-      </RootStack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<RootStack.Navigator
+				screenOptions={{
+					headerShown: false,
+					animationEnabled: false
+				}}
+			>
+				{isAuthenticated ? (
+					<RootStack.Screen name={'MainStack'} component={MainNavigator} />
+				) : (
+					<RootStack.Screen name={'AuthStack'} component={AuthStackNavigator} />
+				)}
+			</RootStack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default App;
---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { View, Text, Image, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const SplashScreen = ({ navigation }) => {
+	useEffect(() => {
+		setTimeout(() => {
+			navigation.navigate('LoginStack');
+		}, 3000);
+	}, []);
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: '#141414'
+			}}
+		>
+			<Image
+				source={require('../Assets/logoplay.png')}
+				style={{ marginBottom: 5, width: 300, resizeMode: 'stretch' }}
+			/>
+		</View>
+	);
+};
+
+export default SplashScreen;
