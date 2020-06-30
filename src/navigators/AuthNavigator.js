a5a54d2f988fffcddcabf22137183115c890523a
fadhil darma putera zm <fadhildarma13@gmail.com>
2020-06-30 12:59:04 +0700

    navigate login

--- src/navigators/AuthNavigator.js
+++ src/navigators/AuthNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import Login from '../screens/Login';
 import Register from '../screens/Register';
@@ -9,28 +9,18 @@ const AuthStack = createStackNavigator();
 const LoginStack = createStackNavigator();
 
 export function AuthStackNavigator() {
-	return (
-		<AuthStack.Navigator
-			mode={'modal'}
-			screenOptions={{
-				headerShown: false
-			}}
-			initialRouteName="Splash"
-		>
-			<AuthStack.Screen name={'Splash'} component={SplashScreen} />
-			<AuthStack.Screen name={'LoginStack'}>
-				{() => (
-					<LoginStack.Navigator
-						mode={'card'}
-						screenOptions={{
-							headerShown: false
-						}}
-					>
-						<LoginStack.Screen name={'Login'} component={Login} />
-					</LoginStack.Navigator>
-				)}
-			</AuthStack.Screen>
-			<AuthStack.Screen name={'Registration'} component={Register} />
-		</AuthStack.Navigator>
-	);
+  return (
+    <AuthStack.Navigator
+      mode={'modal'}
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName="Splash">
+      <AuthStack.Screen name={'Splash'} component={SplashScreen} />
+      <AuthStack.Screen
+        name={'LoginStack'}
+        component={Login}></AuthStack.Screen>
+      <AuthStack.Screen name={'Registration'} component={Register} />
+    </AuthStack.Navigator>
+  );
 }
